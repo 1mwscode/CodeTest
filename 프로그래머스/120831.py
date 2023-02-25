63f7ebb7ece770c9e0f003621791638e139946f8
Lucas <woosoung1993@gmail.com>
2023-02-25 16:15:46 +0900

    Create 120831.py

---
+++ 프로그래머스/120831.py
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg949\cocoartf2708
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 # \uc0\u51677 \u49688 \u51032  \u54633  \u47928 \u51228 \
+\
+# https://school.programmers.co.kr/learn/courses/30/lessons/120831\
+\
+solution = lambda n : int(n/2)**2 +int(n/2)  #\uc0\u46321 \u52264 \u49688 \u50676 \u54633  \u44277 \u49885 }
